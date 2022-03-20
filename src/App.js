4acbc4dce8f8cd8bc725ddd061629e924bac06b5
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-03-21 09:27:10 +1100

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { ContactList } from "./components/ContactList";
+import { SearchFilter } from "./components/SearchFilter";
+import { Title } from "./components/Title";
 
-function App() {
-  return <div>Hello Contact</div>;
-}
-
+const App = () => {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        {/* title section */}
+        <Title />
+        {/* Search and filter section */}
+        <SearchFilter />
+        <hr />
+        {/* user count */}
+        <div className="row">
+          <div className="col">55 users found</div>
+        </div>
+        {/* contact list cards */}
+        <ContactList />
+      </div>
+    </div>
+  );
+};
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Card = () => {
+  return (
+    <div className="col-md-6 col-md-3 py-3">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
